1927b1a09b6e24a320691558b4f4a59df66e1972
William Wang <keser.wang@gmail.com>
2020-01-07 15:16:18 +0800

    Add StaticWebApp and it's FrondEndLib.

---
+++ StaticWebApp/FrondEndLib/DateRangePicker/3.0.5/Custom/DateRangePickerCustom.js
@@ -0,0 +1,33 @@
+﻿var DateRangePickerConfig = {
+    "singleDatePicker": true,
+    "timePicker": true,
+    "timePicker24Hour": true,
+    "timePickerSeconds": true,
+    "locale": {
+        "format": 'YYYY/MM/DD HH:mm:ss',
+        "daysOfWeek": [
+            "日",
+            "一",
+            "二",
+            "三",
+            "四",
+            "五",
+            "六"
+        ],
+        "monthNames": [
+            "一月",
+            "二月",
+            "三月",
+            "四月",
+            "五月",
+            "六月",
+            "七月",
+            "八月",
+            "九月",
+            "十月",
+            "十一月",
+            "十二月"
+        ],
+        "firstDay": 1
+    }
+};
